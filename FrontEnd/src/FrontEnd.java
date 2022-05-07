240ff20074f65f80dfd2b6aaf1e70ecd68c32995
Erez Polak <pwlqrz@gmail.com>
2022-05-08 00:20:23 +0300

    FrontEnd is working just fine

---
+++ FrontEnd/src/FrontEnd.java
@@ -0,0 +1,34 @@
+import java.io.IOException;
+import java.net.Socket;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.UnknownHostException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class FrontEnd {
+    public static void main(String[] args) {
+        try {
+
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(new URI("http://127.0.0.1:8000/hello"))
+                    .GET()
+                    .build();
+
+            HttpResponse<String> response = HttpClient.newBuilder()
+                    .build()
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+
+            System.out.println(response.statusCode());
+
+
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
