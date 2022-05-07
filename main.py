713eaff839153d1f1e1cefc6c7612c8cedbbd0e1
Erez Polak <pwlqrz@gmail.com>
2022-05-08 00:34:51 +0300

    backEnd is working just fine

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    input_string: str
+
 
 app = FastAPI()
 
+n = 0
 
-@app.get("/hello")
+
+@app.get("/n")
 async def hello():
-    return {"message": "hello world"}
+    global n
+    n += 1
+    return {"massage": f"hello world {n}"}
+
+
+@app.post("/n")
+async def set_n(item: Item):
+    global n
+    n = int(item.input_string)
+    return {"massage": f"successfully changed n to {n}"}
