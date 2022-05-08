b1609a1d5772f4567cb8ff67931770111fc1edb8
Erez Polak <pwlqrz@gmail.com>
2022-05-08 20:02:21 +0300

    intelij update

--- FrontEnd/src/Board.java
+++ FrontEnd/src/Board.java
@@ -2,9 +2,6 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -74,7 +71,6 @@ public class Board {
                 }
 
                 response = hc1.send(get_state_request, HttpResponse.BodyHandlers.ofString());
-                response = hc2.send(get_state_request, HttpResponse.BodyHandlers.ofString());
 
                 JSONObject json = (JSONObject) parser.parse(response.body());
 
