588e3f6e1f2453d8deb0f0ce8dca601c35d34f0b
Erez Polak <pwlqrz@gmail.com>
2022-05-09 14:57:47 +0300

    basic graphic presentation

--- FrontEnd/src/Board.java
+++ FrontEnd/src/Board.java
@@ -1,97 +1,54 @@
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
+public class Board extends JFrame {
 
-public class Board {
-//
-//    public static void main(String[] args) throws URISyntaxException {
-//
-//        try {
-//
-//            HttpClient hc1 = HttpClient.newBuilder().build();
-//            HttpClient hc2 = HttpClient.newBuilder().build();
-//
-//            JSONParser parser = new JSONParser();
-//
-//            HttpRequest hand_shake_request = HttpRequest.newBuilder()
-//                    .uri(new URI("http://127.0.0.1:8000/api/hello"))
-//                    .version(HttpClient.Version.HTTP_1_1)
-//                    .GET()
-//                    .build();
-//
-//            HttpRequest get_state_request = HttpRequest.newBuilder()
-//                    .uri(new URI("http://127.0.0.1:8000/api/state"))
-//                    .version(HttpClient.Version.HTTP_1_1)
-//                    .GET()
-//                    .build();
-//
-//            JSONObject direction = new JSONObject();
-//            direction.put("param" , "3");
-//            direction.put("function" , "0");
-//
-//            HttpRequest set_user_direction_request = HttpRequest.newBuilder()
-//                    .uri(new URI("http://127.0.0.1:8000/api/direction"))
-//                    .version(HttpClient.Version.HTTP_1_1)
-//                    .PUT(HttpRequest.BodyPublishers.ofString(direction.toJSONString()))
-//                    .build();
-//
-//            HttpResponse<String> hand_shake_response1 = hc1.send(hand_shake_request, HttpResponse.BodyHandlers.ofString());
-//            HttpResponse<String> hand_shake_response2 = hc2.send(hand_shake_request, HttpResponse.BodyHandlers.ofString());
-//
-//            JSONObject hand_shake_json1 = (JSONObject) parser.parse(hand_shake_response1.body());
-//            JSONObject hand_shake_json2 = (JSONObject) parser.parse(hand_shake_response2.body());
-//
-//            System.out.println(hand_shake_json1.toJSONString());
-//            System.out.println(hand_shake_json2.toJSONString());
-//
-//            HttpResponse<String> response;
-//
-//            for (int i = 0; i < 100; i++) {
-//
-//                if(i == 10){
-//                    response = hc1.send(set_user_direction_request, HttpResponse.BodyHandlers.ofString());
-//
-//                    JSONObject json = (JSONObject) parser.parse(response.body());
-//
-//                    System.out.println(json.toJSONString());
-//                }
-//
-//                if(i == 5){
-//                    response = hc2.send(set_user_direction_request, HttpResponse.BodyHandlers.ofString());
-//
-//                    JSONObject json = (JSONObject) parser.parse(response.body());
-//
-//                    System.out.println(json.toJSONString());
-//                }
-//
-//                response = hc1.send(get_state_request, HttpResponse.BodyHandlers.ofString());
-//
-//                JSONObject json = (JSONObject) parser.parse(response.body());
-//
-//                System.out.println(json.toJSONString());
-//
-//                Thread.sleep(500);
-//
-//            }
-//
-//
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        } catch (ParseException e) {
-//            e.printStackTrace();
-//        }
-//
-//    }
-//
+    private GraphicsClass gc;
+
+    public Board() {
+
+        setBounds(5, 5, 1015, 565);
+
+        gc = new GraphicsClass();
+
+        //adding the graphics class as a component to the frame.
+        add(gc);
+
+        //adding the listeners of the keyboard to the frame as a component too.
+        addKeyListener(new TAdapter());
+
+
+    }
+
+    class TAdapter extends KeyAdapter {
+        public void keyPressed(KeyEvent e) {
+            int keycode = e.getKeyCode();
+            switch (keycode) {
+                case KeyEvent.VK_LEFT -> {
+
+                }
+                case KeyEvent.VK_RIGHT -> {
+
+                }
+                case KeyEvent.VK_UP -> {
+
+                }
+
+                case KeyEvent.VK_DOWN -> {
+
+                }
+
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Board b = new Board();
+        b.setVisible(true);
+    }
 
 }
 
--- FrontEnd/src/GraphicsClass.java
+++ FrontEnd/src/GraphicsClass.java
@@ -1,2 +1,26 @@
-public class GraphicsClass {
+import javax.swing.*;
+import java.awt.*;
+import java.util.Random;
+
+public class GraphicsClass extends JPanel {
+
+    Random r = new Random();
+
+    private int n = 0;
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        n += 1;
+
+        g.setColor(new Color(r.nextInt(0,100),0,0));
+        g.fillOval(100,100,n % 100,n % 100);
+
+        g.setColor(new Color(0,r.nextInt(0,100),0));
+        g.fillOval(500,100,n % 100,n % 100);
+
+        repaint();
+
+    }
+
 }
